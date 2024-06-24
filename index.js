c4c8010dd9967b22e49948865f228d9bf8afd3ee
Nitesh Chauhan <72297535+Nitesh121-maker@users.noreply.github.com>
2024-06-24 12:32:18 +0530

    Commited on 24 at 12:32

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const LocalStrategy = require('passport-local').Strategy;
 const nodemailer = require('nodemailer');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://192.168.1.10:3002'
+}));
 app.use(express.json());
 const fs = require('fs');
 app.use(session({
